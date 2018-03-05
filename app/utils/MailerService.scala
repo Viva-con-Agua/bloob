983bb67b56226582cd4f957bb80bff9c4b5ae919
Johann Sell <sell@informatik.hu-berlin.de>
2018-03-05 08:54:41 +0100

    #1 send one email for every user; no mass mailing

--- app/utils/MailerService.scala
+++ app/utils/MailerService.scala
@@ -22,15 +22,18 @@ class MailerService @Inject() (mailerClient: MailerClient, mailDAO : MailDAO) {
   }
 
   def send(mail: Mail) : Future[Option[Mail]] = {
-    val email = Email(
-      mail.subject,
-      mail.meta.sendingAddress.map(toSender( _ )).getOrElse(toSender( defaultSender )), // TODO: If author is complete user, add possibility to send from users vivaconagua address
-      mail.receiver.toSeq, // TODO: If receiver are complete users, adopt schema: "Miss TO <to@email.com>"
-      // sends text, HTML or both...
-      bodyText = Some(Jsoup.parse(mail.body).text),//Some(scala.xml.XML.loadString(mail.body).text), // TODO: Does this function respects line breaks, etc?
-      bodyHtml = Some(mail.body)
-    )
-    mailerClient.send(email)
+    // every user has te receive her / his own e-mail; no mass mail with readable other addresses and no bcc
+    for(receiver <- mail.receiver) {
+      val email = Email(
+        mail.subject,
+        mail.meta.sendingAddress.map(toSender(_)).getOrElse(toSender(defaultSender)), // TODO: If author is complete user, add possibility to send from users vivaconagua address
+        Seq(receiver), // TODO: If receiver are complete users, adopt schema: "Miss TO <to@email.com>"
+        // sends text, HTML or both...
+        bodyText = Some(Jsoup.parse(mail.body).text), //Some(scala.xml.XML.loadString(mail.body).text), // TODO: Does this function respects line breaks, etc?
+        bodyHtml = Some(mail.body)
+      )
+      mailerClient.send(email)
+    }
     mailDAO.send(mail.id)
   }
 
